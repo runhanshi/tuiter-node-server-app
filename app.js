35059e712f4ba4a734ffa44250cd70ddb110ad9f
runhanshi <ShiRunhan0329@outlook.com>
2022-11-25 13:09:57 -0800

    add a9

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import UserController
     from "./ controllers/users/users-controller.js"
 import TuitsController
     from "./ controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+mongoose.connect('mongodb://localhost:27017/tuiter');
+
 const app = express();
 app.use(cors())
 app.use(express.json());
