7bbe6d510b6a299b8211d7d02a850f26f30c9273
runhanshi <ShiRunhan0329@outlook.com>
2022-11-30 10:52:51 -0800

    add a9

---  controllers/tuits/tuits-schema.js
+++  controllers/tuits/tuits-schema.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
-const schema = mongoose.Schema({
+const { Schema } = mongoose;
+
+const schema = new Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+    dislikes: Number,
+    disliked: Boolean,
+
 }, {collection: 'tuits'});
 export default schema;
