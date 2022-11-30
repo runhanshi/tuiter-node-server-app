191c4e8b904c6d360c2cf8253b74f7c888d7d9ed
runhanshi <ShiRunhan0329@outlook.com>
2022-11-30 15:12:43 -0800

    bug

--- app.js
+++ app.js
@@ -7,13 +7,33 @@ import TuitsController
     from "./ controllers/tuits/tuits-controller.js";
 
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+const CONNECTION_STRING = 'process.env.DB_CONNECTION_STRING'
     || 'mongodb://127.0.0.1:27017/tuiter';
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect('mongodb+srv://runhan:970329@cluster1.3q7rbw3.mongodb.net/tuiter?retryWrites=true&w=majority');
+
 
 const app = express();
 app.use(cors())
 app.use(express.json());
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // const express = require('express')
 // const app = express()
