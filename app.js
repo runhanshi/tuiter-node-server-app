db4458b6ba3c16321224f1846797b06a1bd64402
runhanshi <ShiRunhan0329@outlook.com>
2022-11-30 15:49:54 -0800

    bug13

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const corsOptions ={
     origin:'*',
     credentials:true,
     methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-    headers: 'X-Requested-With,Authorization,content-type',//access-control-allow-credentials:true
+    headers: 'X-Requested-With,content-type',//access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
@@ -31,7 +31,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Authorization,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Pass to next layer of middleware
     next();
