dc2fb4daf0365af975a20e935c397e3b795b94ab
runhanshi <ShiRunhan0329@outlook.com>
2022-11-30 15:31:08 -0800

    bug8

--- app.js
+++ app.js
@@ -22,6 +22,18 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 // const express = require('express')
 // const app = express()
 // app.get('/hello', (req, res) => {res.send('Life is good!')})
